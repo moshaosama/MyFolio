06fc5b0294c25577e8fb2750f460c7f2791c1f03
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 18:20:59 +0300

    modify: updated Home page content or structure

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,13 @@
+import Hero from "../../Features/Home/Components/Hero";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="h-[83.1vh] py-20 bg-gradient-to-l to-blue-900  from-indigo-950">
+        <Hero />
+      </div>
+    </>
+  );
 };
 
 export default Home;

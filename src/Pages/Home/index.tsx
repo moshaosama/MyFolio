21e985a8b7bea0808998b4d2edfd9eb3af3faf4d
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 20:13:03 +0300

    modify: updated Home page with new components or logic

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Hero from "../../Features/Home/Components/Hero";
+import PopUpSupport from "../../Features/Home/Components/PopUpSupport";
 
 const Home = () => {
   return (
     <>
       <div className="h-[83.1vh] py-20 bg-gradient-to-l to-blue-900  from-indigo-950">
         <Hero />
+        <PopUpSupport />
       </div>
     </>
   );

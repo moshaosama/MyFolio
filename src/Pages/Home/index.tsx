69eb00cbe85c0584f47829e4fd6767a70747b5f2
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 00:58:13 +0300

    modify: updated Home page to integrate PopUpCard or layout changes

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Hero from "../../Features/Home/Components/Hero";
 import PopUpSupport from "../../Features/Home/Components/PopUpSupport";
+import PopUpTemplates from "../../Features/Templates/Components/PopUpTemplates";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div className="h-[83.1vh] py-20 bg-gradient-to-l to-blue-900  from-indigo-950">
         <Hero />
         <PopUpSupport />
+        <PopUpTemplates />
       </div>
     </>
   );

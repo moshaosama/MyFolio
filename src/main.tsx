4f9773f3b8d6a600b611f0ac984431e1eccad413
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 01:18:23 +0300

    modify: updated main.tsx with new context providers or components

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import OpenPopUpSupportProvider from "./Features/Home/Context/useOpenPopUpSupport.tsx";
+import OpenPopUpTemplateProvider from "./Features/Templates/Context/useOpenPopUpTemplate.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <OpenPopUpSupportProvider>
-      <App />
+      <OpenPopUpTemplateProvider>
+        <App />
+      </OpenPopUpTemplateProvider>
     </OpenPopUpSupportProvider>
   </StrictMode>
 );

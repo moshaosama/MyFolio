de636dc2b8d4d922b6750f7eb219da133dce611a
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 20:32:54 +0300

    modify: updated CopyRight layout or text

--- src/Layouts/CopyRight/index.tsx
+++ src/Layouts/CopyRight/index.tsx
@@ -1,6 +1,6 @@
 const CopyRight = () => {
   return (
-    <div className="bg-gradient-to-l to-blue-900 text-white border-t-1 py-6 border-gray-500 from-indigo-950 text-center">
+    <div className="bg-gradient-to-l to-blue-900 text-white border-t-1 py-6 max-sm:py-[0.67pc] border-gray-500 from-indigo-950 text-center">
       © 2024 Selfolio. Built with ❤️ by{" "}
       <a
         href="https://github.com/moshaosama"

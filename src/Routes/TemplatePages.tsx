e7a90c2df7cf077b58857d2cf64928bd106117bd
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 02:13:39 +0300

    modify: updated TemplatePages routing logic or component mapping

--- src/Routes/TemplatePages.tsx
+++ src/Routes/TemplatePages.tsx
@@ -1,9 +1,12 @@
 import SplitLayoutTemplate from "../Layouts/Template/SplitLayout";
 import Template from "../Pages/Templates";
+import WithLoader from "../Utils/WithLoader";
+
+const SplitLayoutLoader = WithLoader(SplitLayoutTemplate);
 
 export const TemplatePages = {
   path: "template",
-  element: <SplitLayoutTemplate />,
+  element: <SplitLayoutLoader />,
   children: [
     {
       index: true,

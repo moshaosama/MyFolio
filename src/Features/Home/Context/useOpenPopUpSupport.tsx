3ef98ae0e6a4752ead1f100089a0789fe8b746de
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 20:13:07 +0300

    add: added context for managing Home state

---
+++ src/Features/Home/Context/useOpenPopUpSupport.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+
+interface DataContext {
+  isOpen: boolean;
+  triggerOpenPopUp: () => void;
+}
+
+interface OpenPopUpSupportProviderProps {
+  children: ReactNode;
+}
+
+const OpenPopUpSupportContext = createContext<null | DataContext>(null);
+
+const OpenPopUpSupportProvider = ({
+  children,
+}: OpenPopUpSupportProviderProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const triggerOpenPopUp = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <OpenPopUpSupportContext.Provider value={{ isOpen, triggerOpenPopUp }}>
+      {children}
+    </OpenPopUpSupportContext.Provider>
+  );
+};
+
+export default OpenPopUpSupportProvider;
+
+export const useOpenPopUpSupportContext = () => {
+  const context = useContext(OpenPopUpSupportContext);
+  if (!context) {
+    throw new Error(
+      "useOpenPopUpSupportContext must be used within an OpenPopUpSupportProvider"
+    );
+  }
+  return context;
+};

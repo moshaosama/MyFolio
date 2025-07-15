68bea481e8eaf830c1579b3c1bac7c8f40d9fd14
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 20:32:46 +0300

    modify: adjusted Hero section content or structure

--- src/Features/Home/Components/Hero.tsx
+++ src/Features/Home/Components/Hero.tsx
@@ -2,19 +2,19 @@ const Hero = () => {
   return (
     <>
       <div className="flex flex-col items-center gap-5 justify-center text-white">
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-6xl font-bold max-sm:text-4xl max-sm:text-center">
           Create Your Perfect{" "}
           <span className="bg-gradient-to-l from-purple-600 to-blue-600 to-60% bg-clip-text text-transparent">
             Portfolio
           </span>
         </h1>
 
-        <p className="text-xl text-center w-1/2 text-[#d1d5dc]">
+        <p className="text-xl text-center w-1/2 max-sm:w-[23pc] text-[#d1d5dc]">
           Build a stunning portfolio in minutes with our customizable templates.
           Export as a complete React project and deploy instantly.
         </p>
 
-        <div className="flex items-center gap-6">
+        <div className="flex max-sm:flex-col items-center gap-6">
           <button className="cursor-pointer bg-gradient-to-l from-purple-600 hover:from-purple-800 to-blue-600 hover:to-blue-800 hover:scale-105 transition-all duration-300 p-4 w-40 rounded-xl font-semibold text-xl">
             Get Started
           </button>

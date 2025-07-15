796346f6d6e36e2163d73529b83c8bbf0928edb2
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 17:56:37 +0300

    modify: improved NavbarHome layout and behavior

--- src/Layouts/NavbarHome/index.tsx
+++ src/Layouts/NavbarHome/index.tsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import cn from "../../libs/cn";
 import Container from "../../Styles";
 
@@ -5,13 +6,27 @@ const NavbarHome = () => {
   return (
     <>
       <div className="bg-gradient-to-l py-5 to-indigo-950 from-blue-900">
-        <div className={cn(Container, "flex justify-between")}>
+        <div className={cn(Container, "flex justify-between items-center")}>
           <div>
             <h1 className="text-white font-bold text-2xl">Myfolio</h1>
           </div>
 
-          <div>
-            
+          <div className="flex items-center gap-5 text-[#d1d5dc]">
+            <a href="https://github.com/moshaosama" target="_blank">
+              <FaGithub
+                size={30}
+                className="hover:bg-[#d1d5dc52] p-1 rounded-xl cursor-pointer"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/thisfekry/" target="_blank">
+              <FaLinkedin
+                size={30}
+                className="hover:bg-[#d1d5dc52] p-1 rounded-xl cursor-pointer"
+              />
+            </a>
+            <h1 className="hover:bg-[#d1d5dc52] p-2 rounded-xl cursor-pointer">
+              ☕ Support us
+            </h1>
           </div>
         </div>
       </div>

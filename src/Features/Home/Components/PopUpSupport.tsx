04ca8272b34455772ab79936aae5615ac45a0405
moshaosama <mohamedOSFekry@gmail.com>
2025-07-15 20:21:56 +0300

    modify: updated PopUpSupport component UI or behavior

--- src/Features/Home/Components/PopUpSupport.tsx
+++ src/Features/Home/Components/PopUpSupport.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import PopUpCard from "../../../Components/PopUpCard";
 import cn from "../../../libs/cn";
 import { useOpenPopUpSupportContext } from "../Context/useOpenPopUpSupport";
+import { FaCheck } from "react-icons/fa";
 
 const PopUpSupport = () => {
   const { isOpen } = useOpenPopUpSupportContext();
+
+  const [isCopyMobile, setIsCopyMobile] = useState(false);
+  const [isCopyEmail, setIsCopyEmail] = useState(false);
+
+  const triggerCopyMobile = () => {
+    setIsCopyMobile(true);
+
+    setTimeout(() => {
+      setIsCopyMobile(false);
+    }, 2000);
+  };
+
+  const triggerCopyEmail = () => {
+    setIsCopyEmail(true);
+
+    setTimeout(() => {
+      setIsCopyEmail(false);
+    }, 2000);
+  };
   return (
     <>
       <div
@@ -36,8 +57,22 @@ Thank you for supporting Selfolio! Your contribution helps us maintain and impro
               <h1>mohamedOSFekry@gmail.com</h1>
             </div>
 
-            <div className="bg-gradient-to-l p-3 text-center  rounded-xl to-blue-500 from-blue-300 cursor-pointer hover:to-blue-600 hover:from-blue-400">
-              <h1 className="font-bold">📋 Copy Airtm Account</h1>
+            <div
+              onClick={() => {
+                navigator.clipboard.writeText("mohamedOSFekry@gmail.com");
+                triggerCopyEmail();
+              }}
+              className="bg-gradient-to-l p-3 text-center  rounded-xl to-blue-500 from-blue-300 cursor-pointer hover:to-blue-600 hover:from-blue-400"
+            >
+              <h1 className="font-bold">
+                {isCopyEmail ? (
+                  <i className="flex items-center gap-5 justify-center">
+                    <FaCheck /> Copied!
+                  </i>
+                ) : (
+                  "📋 Copy Airtm Account"
+                )}
+              </h1>
             </div>
           </div>
 
@@ -58,8 +93,22 @@ Thank you for supporting Selfolio! Your contribution helps us maintain and impro
               <h1>+20 1004365707</h1>
             </div>
 
-            <div className="bg-gradient-to-l p-3 text-center  rounded-xl to-purple-800 from-puto-purple-700 cursor-pointer hover:to-purple-900 hover:from-puto-purple-800">
-              <h1 className="font-bold">📋 Copy Vodafone Cash Account</h1>
+            <div
+              onClick={() => {
+                navigator.clipboard.writeText("+20 1004365707");
+                triggerCopyMobile();
+              }}
+              className="bg-gradient-to-l p-3 text-center  rounded-xl to-purple-800 from-puto-purple-700 cursor-pointer hover:to-purple-900 hover:from-puto-purple-800"
+            >
+              <h1 className="font-bold">
+                {isCopyMobile ? (
+                  <i className="flex items-center gap-5 justify-center">
+                    <FaCheck /> Copied!
+                  </i>
+                ) : (
+                  "📋 Copy Vodafone Cash Account"
+                )}
+              </h1>
             </div>
           </div>
         </PopUpCard>

be7b6a9a774f7c8502ec8880f6b3b1363059f78f
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 16:27:34 +0300

    modify: updated main.tsx with new providers or render logic

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import OpenPopUpSupportProvider from "./Features/Home/Context/useOpenPopUpSupport.tsx";
 import OpenPopUpTemplateProvider from "./Features/Templates/Context/useOpenPopUpTemplate.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <OpenPopUpSupportProvider>
       <OpenPopUpTemplateProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </OpenPopUpTemplateProvider>
     </OpenPopUpSupportProvider>
   </StrictMode>

e7e7139641acd7d04057eda51419875416bb4428
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 20:49:44 +0300

    delete: removed unused useGetUser hook

--- src/Hooks/useGetUser.ts
+++
@@ -1,8 +0,0 @@
-const useGetUser = () => {
-  const userString = window.localStorage.getItem("User");
-  const User = userString ? JSON.parse(userString) : null;
-
-  return { User };
-};
-
-export default useGetUser;

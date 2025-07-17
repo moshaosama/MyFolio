c1395add715b0eb3b0f3eeabbc8af6759d200321
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 19:28:45 +0300

    feat: add/edit PopUpEditTags component

--- src/Features/Portfolio/Components/PopUpEditTags.tsx
+++ src/Features/Portfolio/Components/PopUpEditTags.tsx
@@ -4,6 +4,7 @@ import useEditTags from "../Hooks/useEditTags";
 const PopUpEditTags = () => {
   const { tagsValue } = useEditTags();
   const { TriggerOpenEditTags } = useOpenEditTagsContext();
+  const { register, handleSubmit, handleEditTags } = useEditTags();
   return (
     <>
       <div className="fixed flex justify-center items-center h-full inset-0 top-0 w-full bg-black/60">
@@ -13,10 +14,11 @@ const PopUpEditTags = () => {
               <h1 className="text-2xl font-bold">Edit Tags</h1>
               <p className="text-slate-500">Enter tags separated by commas:</p>
             </div>
-            <form action="">
+            <form onSubmit={handleSubmit(handleEditTags)}>
               <textarea
                 className="border-1 h-20 w-full p-2 border-gray-500 rounded-xl"
-                value={tagsValue}
+                defaultValue={tagsValue}
+                {...register("Tags", { required: true })}
               />
 
               <p className="flex justify-end  gap-4 my-2">

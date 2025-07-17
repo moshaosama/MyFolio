d256ea76424bb1f820ac2265c3a63629c0dee93b
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 04:35:08 +0300

    feat: create About component

---
+++ src/Features/Portfolio/Components/About.tsx
@@ -0,0 +1,17 @@
+import useGetAnimationText from "../../../Hooks/useGetAnimationText";
+
+const About = () => {
+  const { startText } = useGetAnimationText([
+    "Front-end Developer",
+    "Bac-end Developer",
+  ]);
+  return (
+    <>
+      <div className="text-white p-2 font-bold text-lg bg-black w-60 rounded-xl">
+        <h1>✨ {startText}</h1>
+      </div>
+    </>
+  );
+};
+
+export default About;

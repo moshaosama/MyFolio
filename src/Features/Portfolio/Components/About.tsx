a83facd5fd9b4e6fee099363d2c55489d8e8eda5
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 20:30:59 +0300

    feat: enhance About section UI and content

--- src/Features/Portfolio/Components/About.tsx
+++ src/Features/Portfolio/Components/About.tsx
@@ -3,14 +3,13 @@ import useGetAnimationText from "../../../Hooks/useGetAnimationText";
 import useGetUser from "../Hooks/useGetUser";
 import PopUpEditTags from "./PopUpEditTags";
 import { useOpenEditTagsContext } from "../Context/EditTagsModelContext";
+import useEditname from "../Hooks/useEditname";
 
 const About = () => {
   const { User } = useGetUser();
-  console.log(JSON.parse(User?.Tags));
-
   const { startText } = useGetAnimationText(JSON.parse(User?.Tags));
-
   const { isOpenEditTags, TriggerOpenEditTags } = useOpenEditTagsContext();
+  const { register, handlEditName, handleSubmit } = useEditname();
   return (
     <>
       <div className="relative text-white p-2 px-4 font-bold text-lg bg-black w-48 rounded-full ">
@@ -26,10 +25,14 @@ const About = () => {
       <div className="mt-3">
         <h1 className="text-white text-4xl font-bold">Hello I am</h1>
         <div className="w-96 flex break-words mt-2">
-          <input
-            value={User?.Name}
-            className="text-5xl font-extrabold bg-gradient-to-l bg-clip-text w-[26pc] from-[#2a7b9b] to-[#57c785] text-transparent focus:outline-0 focus:border-b-2 focus:border-b-purple-400 wrap-break-word"
-          />
+          <form>
+            <input
+              defaultValue={User?.Name}
+              {...register("Name", { required: true })}
+              onMouseLeave={handleSubmit(handlEditName)}
+              className="text-5xl font-extrabold bg-gradient-to-l bg-clip-text w-[26pc] from-[#2a7b9b] to-[#57c785] text-transparent focus:outline-0 focus:border-b-2 focus:border-b-purple-400 wrap-break-word"
+            />
+          </form>
 
           <div className="cursor-pointer">
             <CiShare1 color="white" size={30} />

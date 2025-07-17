8969a1b816c3a29eb413657e3a8f579a271a3dc7
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 18:31:01 +0300

    refactor: enhance DataUserIdService logic

--- src/Api/DataByUserId/DataUserIdService.ts
+++ src/Api/DataByUserId/DataUserIdService.ts
@@ -41,6 +41,20 @@ class DataUserIdService {
       return thunkApi.rejectWithValue(err);
     }
   }
+
+  async EditTags(data?: { Tags: string; userId: number }, thunkApi?: any) {
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/edit-tags/${data.userId}`,
+        {
+          Tags: data?.Tags,
+        }
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
 }
 
 export const dataUserIdService = new DataUserIdService();

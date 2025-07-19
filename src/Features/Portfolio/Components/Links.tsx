93e4149dec4a3ceb711d9cfc6ecf37bb0c555083
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:32:30 +0300

    refactor: update Links component to support new link models

--- src/Features/Portfolio/Components/Links.tsx
+++ src/Features/Portfolio/Components/Links.tsx
@@ -1,30 +1,35 @@
 import { CiShare1 } from "react-icons/ci";
 import { GoArrowRight } from "react-icons/go";
 import LinksIcons from "../../../Components/LinksIcons";
-import EditLinkModel from "../../../Components/EditLinkModel";
+import { useOpenEditTagsContext } from "../Context/EditTagsModelContext";
+import EditLinksModels from "./EditLinksModels";
+import useGetUser from "../Hooks/useGetUser";
 
 const Links = () => {
+  const { TriggerOpenEditResume } = useOpenEditTagsContext();
+  const { User } = useGetUser();
   return (
     <>
       <div className="flex items-center justify-between">
         <button className="group relative bg-gradient-to-l p-5 w-60 text-white hover:shadow-lg shadow-[#2a7b9b] hover:scale-105 transition-all duration-300 font-bold rounded-xl cursor-pointer  text-lg my-10 from-[#2a7b9b] to-[#57c785]">
-          <div className="flex gap-4 items-center justify-center">
-            <p>View My Resume</p>
-            <i>
-              <GoArrowRight />
-            </i>
-          </div>
+          <a href={User?.ResumeLink}>
+            <div className="flex gap-4 items-center justify-center">
+              <p>View My Resume</p>
+              <i>
+                <GoArrowRight />
+              </i>
+            </div>
+          </a>
 
-          <i className="absolute hidden group-hover:block -right-4 -top-6">
+          <i
+            onClick={TriggerOpenEditResume}
+            className="absolute hidden group-hover:block -right-4 -top-6"
+          >
             <CiShare1 color="white" size={25} className="cursor-pointer" />
           </i>
         </button>
         <LinksIcons />
-        {/* <EditLinkModel
-          Title="Edit Resume Button"
-          Label="Resume URL"
-          DefaultValue=""
-        /> */}
+        <EditLinksModels />
       </div>
     </>
   );

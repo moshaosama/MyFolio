861b012fb5319ed7350fba90352104c569f116c3
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:32:29 +0300

    refactor: adjust LinksIcons rendering and props

--- src/Components/LinksIcons.tsx
+++ src/Components/LinksIcons.tsx
@@ -1,54 +1,84 @@
 import { CiShare1 } from "react-icons/ci";
 import { FaGithub, FaLinkedin, FaTwitter, FaMedium } from "react-icons/fa";
+import { useOpenEditTagsContext } from "../Features/Portfolio/Context/EditTagsModelContext";
+import { Link } from "react-router";
+import useGetUser from "../Features/Portfolio/Hooks/useGetUser";
 
 const LinksIcons = () => {
+  const {
+    TriggerOpenEditGithub,
+    TriggerOpenEditLinkedin,
+    TriggerOpenEditMedium,
+    TriggerOpenEditTwitter,
+  } = useOpenEditTagsContext();
+  const { User } = useGetUser();
   return (
     <>
       <div className="flex gap-5 mr-10">
         <i className="relative group">
-          <FaGithub
-            size={45}
-            color="white"
-            className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
-          />
+          <a href={User?.GitHubLink}>
+            <FaGithub
+              size={45}
+              color="white"
+              className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
+            />
+          </a>
 
-          <i className="absolute hidden group-hover:block -left-4 -top-6">
+          <i
+            onClick={TriggerOpenEditGithub}
+            className="absolute hidden group-hover:block -left-4 -top-6"
+          >
             <CiShare1 color="white" size={25} className="cursor-pointer" />
           </i>
         </i>
 
         <i className="relative group">
-          <FaLinkedin
-            size={45}
-            color="white"
-            className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
-          />
+          <a href={User?.LinkedInLink}>
+            <FaLinkedin
+              size={45}
+              color="white"
+              className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
+            />
+          </a>
 
-          <i className="absolute hidden group-hover:block -left-4 -top-6">
+          <i
+            onClick={TriggerOpenEditLinkedin}
+            className="absolute hidden group-hover:block -left-4 -top-6"
+          >
             <CiShare1 color="white" size={25} className="cursor-pointer" />
           </i>
         </i>
 
         <i className="relative group">
-          <FaTwitter
-            size={45}
-            color="white"
-            className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
-          />
+          <a href={User?.TwitterLink}>
+            <FaTwitter
+              size={45}
+              color="white"
+              className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
+            />
+          </a>
 
-          <i className="absolute hidden group-hover:block -left-4 -top-6">
+          <i
+            onClick={TriggerOpenEditTwitter}
+            className="absolute hidden group-hover:block -left-4 -top-6"
+          >
             <CiShare1 color="white" size={25} className="cursor-pointer" />
           </i>
         </i>
 
         <i className="relative group">
-          <FaMedium
-            size={45}
-            color="white"
-            className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
-          />
+          <a href={User?.MediumLink}>
+            <FaMedium
+              size={45}
+              color="white"
+              className="hover:bg-[#d1d5dc52] transition-all duration-300 p-1 rounded-xl cursor-pointer"
+            />
+          </a>
 
-          <i className="absolute hidden group-hover:block -left-4 -top-6">
+          <i
+            onClick={TriggerOpenEditMedium}
+            className="absolute hidden group-hover:block -left-4 -top-6"
+          >
             <CiShare1 color="white" size={25} className="cursor-pointer" />
           </i>
         </i>

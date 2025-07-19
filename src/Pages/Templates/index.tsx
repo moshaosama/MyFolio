67b1e37b576431589db4db534163d16ab5a7b816
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 23:39:10 +0300

    refactor: integrate project features into main templates page

--- src/Pages/Templates/index.tsx
+++ src/Pages/Templates/index.tsx
@@ -2,6 +2,8 @@ import Hero from "../../Features/Portfolio/Components/Hero";
 import EditTagsModelProvider from "../../Features/Portfolio/Context/EditTagsModelContext";
 import ProjectsCards from "../../Features/Projects/Components/ProjectsCards";
 import ProjectsHeader from "../../Features/Projects/Components/ProjectsHeader";
+import SkillsHeader from "../../Features/Skills/Components/SkillsHeader";
+import SkillsRender from "../../Features/Skills/Components/SkillsRender";
 const Template = () => {
   return (
     <>
@@ -10,6 +12,8 @@ const Template = () => {
           <Hero />
           <ProjectsHeader />
           <ProjectsCards />
+          <SkillsHeader />
+          <SkillsRender />
         </EditTagsModelProvider>
       </div>
     </>

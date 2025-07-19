afd20be61de485577b9ea53816102a8323e36413
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:32:30 +0300

    refactor: update EditTagsModelContext for improved tag handling

--- src/Features/Portfolio/Context/EditTagsModelContext.tsx
+++ src/Features/Portfolio/Context/EditTagsModelContext.tsx
@@ -2,7 +2,17 @@ import { createContext, useContext, useState, type ReactNode } from "react";
 
 interface dataContext {
   isOpenEditTags: boolean;
+  isOpenEditGithub: boolean;
+  isOpenEditLinkedin: boolean;
+  isOpenEditTwitter: boolean;
+  isOpenEditMedium: boolean;
+  TriggerOpenEditGithub: () => void;
+  TriggerOpenEditLinkedin: () => void;
+  TriggerOpenEditTwitter: () => void;
+  TriggerOpenEditMedium: () => void;
+  isOpenEditResume: boolean;
   TriggerOpenEditTags: () => void;
+  TriggerOpenEditResume: () => void;
 }
 
 interface EditTagsModelProviderProps {
@@ -13,13 +23,51 @@ const EditTagsModelContext = createContext<null | dataContext>(null);
 
 const EditTagsModelProvider = ({ children }: EditTagsModelProviderProps) => {
   const [isOpenEditTags, setIsOpenEditTags] = useState(false);
+  const [isOpenEditResume, setIsOpenEditResume] = useState(false);
+
+  const [isOpenEditGithub, setIsOpenEditGithub] = useState(false);
+  const [isOpenEditLinkedin, setIsOpenEditLinkedin] = useState(false);
+  const [isOpenEditTwitter, setIsOpenEditTwitter] = useState(false);
+  const [isOpenEditMedium, setIsOpenEditMedium] = useState(false);
 
   const TriggerOpenEditTags = () => {
     setIsOpenEditTags(!isOpenEditTags);
   };
 
+  const TriggerOpenEditResume = () => {
+    setIsOpenEditResume(!isOpenEditResume);
+  };
+
+  const TriggerOpenEditGithub = () => {
+    setIsOpenEditGithub(!isOpenEditGithub);
+  };
+  const TriggerOpenEditLinkedin = () => {
+    setIsOpenEditLinkedin(!isOpenEditLinkedin);
+  };
+  const TriggerOpenEditTwitter = () => {
+    setIsOpenEditTwitter(!isOpenEditTwitter);
+  };
+  const TriggerOpenEditMedium = () => {
+    setIsOpenEditMedium(!isOpenEditMedium);
+  };
+
   return (
-    <EditTagsModelContext value={{ isOpenEditTags, TriggerOpenEditTags }}>
+    <EditTagsModelContext
+      value={{
+        isOpenEditTags,
+        TriggerOpenEditTags,
+        isOpenEditResume,
+        TriggerOpenEditResume,
+        isOpenEditGithub,
+        isOpenEditLinkedin,
+        isOpenEditTwitter,
+        isOpenEditMedium,
+        TriggerOpenEditGithub,
+        TriggerOpenEditLinkedin,
+        TriggerOpenEditTwitter,
+        TriggerOpenEditMedium,
+      }}
+    >
       {children}
     </EditTagsModelContext>
   );

77eaff4ff47b07f0940ff742f62c5cb7d9b8d80e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 19:20:23 +0300

    refactor: update ProjectService logic for consistency and improved handling

--- src/Api/Projects/ProjectService.ts
+++ src/Api/Projects/ProjectService.ts
@@ -2,11 +2,33 @@ import axios from "axios";
 import { ParentService } from "../parentService";
 
 class ProjectService extends ParentService {
-  CreateData(data?: any, thunkApi?: any): void {}
+  async CreateData(user_id?: any, thunkApi?: any) {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/create-projects/${user_id}`
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
   async GetData(user_id?: any, thunkApi?: any) {
     try {
       const response = await axios.get(
         `http://localhost:3000/get-projects/${user_id}`
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async DeleteProject(project_id?: number, thunkApi?: any) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/delete-project/${project_id}`
       );
 
       return response.data;

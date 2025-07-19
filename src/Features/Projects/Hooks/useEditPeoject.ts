9120ef5fc2c9835390f6eb4678b2bfad6c20d6b2
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 23:39:09 +0300

    feat: implement edit project hook

--- src/Features/Projects/Hooks/useEditPeoject.ts
+++ src/Features/Projects/Hooks/useEditPeoject.ts
@@ -1,16 +1,50 @@
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { projectService } from "../../../Api/Projects/ProjectService";
+import useGetProjects from "./useGetProjects";
 
 const useEditPeoject = () => {
-  const [isOpenEdit, setIsOpenEdit] = useState(false);
-  const handleTriggerOpenEdit = () => {
-    setIsOpenEdit(true);
+  const [isOpenEdit, setIsOpenEdit] = useState<number | null>(null);
+  const { refetch } = useGetProjects();
+  const { register, handleSubmit, reset } = useForm();
+  const { mutate } = useMutation({
+    mutationKey: ["editProfile"],
+    mutationFn: projectService.EditProject,
+    onSuccess: () => {
+      refetch();
+      setIsOpenEdit(null);
+      reset();
+    },
+  });
+
+  const handleEditProject = (data: any) => {
+    mutate({
+      title: data.title,
+      description: data.description,
+      githubLink: data.githubLink,
+      liveDemoLink: data.liveDemoLink,
+      project_id: isOpenEdit as number,
+      skills: JSON.parse(data.skills),
+    });
+  };
+  const handleTriggerOpenEdit = (projectId: number) => {
+    setIsOpenEdit(projectId);
   };
 
   const handleCloseOpenEdit = () => {
-    setIsOpenEdit(false);
+    setIsOpenEdit(null);
+    reset();
   };
 
-  return { isOpenEdit, handleTriggerOpenEdit, handleCloseOpenEdit };
+  return {
+    isOpenEdit,
+    handleTriggerOpenEdit,
+    handleCloseOpenEdit,
+    register,
+    handleSubmit,
+    handleEditProject,
+  };
 };
 
 export default useEditPeoject;

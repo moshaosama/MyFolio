9949eec55a94f9ded03862054aa5d31110ab55b6
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 23:39:10 +0300

    refactor: update ProjectService with new API endpoints

--- src/Api/Projects/ProjectService.ts
+++ src/Api/Projects/ProjectService.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { ParentService } from "../parentService";
 
+interface DataEdit {
+  project_id: number;
+  title: string;
+  description: string;
+  skills: string;
+  githubLink: string;
+  liveDemoLink: string;
+}
+
 class ProjectService extends ParentService {
   async CreateData(user_id?: any, thunkApi?: any) {
     try {
@@ -29,6 +38,25 @@ class ProjectService extends ParentService {
     try {
       const response = await axios.delete(
         `http://localhost:3000/delete-project/${project_id}`
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async EditProject(data?: DataEdit, thunkApi?: any) {
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/edit-project/${data?.project_id}`,
+        {
+          title: data?.title,
+          description: data?.description,
+          skills: data?.skills,
+          githubLink: data?.githubLink,
+          liveDemoLink: data?.liveDemoLink,
+        }
       );
 
       return response.data;

4743ad09e83fea75de70103be399af1b7cb0e6cd
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:22:10 +0300

    chore(templates): update templates index file

--- src/Pages/Templates/index.tsx
+++ src/Pages/Templates/index.tsx
@@ -1,19 +1,43 @@
+import ContactHeader from "../../Features/Contact/Components/ContactHeader";
+import ContactRenders from "../../Features/Contact/Components/ContactRenders";
+import ExperienceHeader from "../../Features/Experience/Components/ExperienceHeader";
 import Hero from "../../Features/Portfolio/Components/Hero";
 import EditTagsModelProvider from "../../Features/Portfolio/Context/EditTagsModelContext";
 import ProjectsCards from "../../Features/Projects/Components/ProjectsCards";
 import ProjectsHeader from "../../Features/Projects/Components/ProjectsHeader";
 import SkillsHeader from "../../Features/Skills/Components/SkillsHeader";
 import SkillsRender from "../../Features/Skills/Components/SkillsRender";
+import OpenSkillProvider from "../../Features/Skills/Context/OpenEditSkillContext";
+import useNavbarLinks from "../../Hooks/useNavbarLinks";
 const Template = () => {
+  const { isFound } = useNavbarLinks();
+
   return (
     <>
       <div className="bg-[#0f0d14]">
         <EditTagsModelProvider>
-          <Hero />
-          <ProjectsHeader />
-          <ProjectsCards />
-          <SkillsHeader />
-          <SkillsRender />
+          <OpenSkillProvider>
+            {isFound.About && <Hero />}
+            {isFound.Projects && (
+              <>
+                <ProjectsHeader />
+                <ProjectsCards />
+              </>
+            )}
+            {isFound.Skills && (
+              <>
+                <SkillsHeader />
+                <SkillsRender />
+              </>
+            )}
+            {isFound.Experience && <ExperienceHeader />}
+            {isFound.Contact && (
+              <>
+                <ContactHeader />
+                <ContactRenders />
+              </>
+            )}
+          </OpenSkillProvider>
         </EditTagsModelProvider>
       </div>
     </>

50d400626f665e63a1e295c32aba54823be41dc4
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:22:09 +0300

    refactor(skills): enhance rendering logic in SkillsRender

--- src/Features/Skills/Components/SkillsRender.tsx
+++ src/Features/Skills/Components/SkillsRender.tsx
@@ -1,28 +1,57 @@
+import { MdDelete } from "react-icons/md";
 import cn from "../../../libs/cn";
 import Container from "../../../Styles";
 import useGetSkill from "../Hooks/useGetSkill";
+import { useOpenSkillContext } from "../Context/OpenEditSkillContext";
+import { IoMdAdd } from "react-icons/io";
+import useDeleteSkill from "../Hooks/useDeletSkill";
+import AddSkillModel from "./AddSkillModel";
 
 const SkillsRender = () => {
   const { Skills } = useGetSkill();
-
+  const { isOpenEditModel, isOpenAddModel, handleTriggerOpenAddModel } =
+    useOpenSkillContext();
+  const { handleDeleteSkill } = useDeleteSkill();
   return (
     <>
       <div className={cn(Container)}>
-        <div className="mx-28 max-sm:mx-0 py-10 w-[67pc] max-sm:w-full grid grid-cols-6 max-sm:grid-cols-2 gap-10">
+        <div className="mx-28 max-sm:mx-0 py-10 w-[67pc] max-sm:w-full grid grid-cols-6 relative max-sm:grid-cols-2 gap-10">
           {Skills?.skills?.map(
             (skill: { skills: string; image: string; id: number }) => (
               <div
                 key={skill.id}
-                className="w-24 rounded-xl hover:shadow-xl transition-all duration-500 hover:shadow-white h-24 hover:bg-slate-700 flex justify-center items-center group"
+                className=" relative rounded-xl hover:shadow-xl transition-all duration-500 hover:shadow-white h-24 hover:bg-slate-700 flex justify-center items-center group"
               >
                 <h1 className="text-white font-bold text-xl group-hover:scale-110 transition-all duration-500">
                   {skill.skills}
                 </h1>
+
+                {isOpenEditModel && (
+                  <MdDelete
+                    onClick={() => handleDeleteSkill(skill.id)}
+                    color="white"
+                    size={30}
+                    className="bg-red-500 absolute top-0 right-0 p-1 rounded-full cursor-pointer hover:bg-red-800"
+                  />
+                )}
               </div>
             )
           )}
+          {isOpenEditModel && (
+            <div
+              onClick={handleTriggerOpenAddModel}
+              className=" relative rounded-xl cursor-pointer hover:shadow-xl transition-all duration-500 hover:shadow-white h-24 hover:bg-slate-700 flex flex-col justify-center items-center group"
+            >
+              <i>
+                <IoMdAdd color="blue" size={50} />
+              </i>
+              <h1 className="text-white font-bold text-3xl">Add Skill</h1>
+            </div>
+          )}
         </div>
       </div>
+
+      {isOpenAddModel && <AddSkillModel />}
     </>
   );
 };

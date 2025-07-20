be70e7b5cab4b6007ea7ad6d04ae4569913f2088
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:22:09 +0300

    refactor(api): optimize SkillService methods

--- src/Api/Skills/skillService.ts
+++ src/Api/Skills/skillService.ts
@@ -2,11 +2,35 @@ import axios from "axios";
 import { ParentService } from "../parentService";
 
 class SkillService extends ParentService {
-  CreateData(): void {}
+  async CreateData(
+    data?: { skill_name: string; userId: number },
+    thunkApi?: any
+  ) {
+    try {
+      const response = await axios.post("http://localhost:3000/create-skill", {
+        skill_name: data?.skill_name,
+        userId: data?.userId,
+      });
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
   async GetData(user_id?: number, thunkApi?: any) {
     try {
       const response = await axios.get(
-        `https://myfolioserver-production.up.railway.app/get-skills/${user_id}`
+        `http://localhost:3000/get-skills/${user_id}`
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async DeleteSkill(skill_id?: number, thunkApi?: any) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/delete-skill/${skill_id}`
       );
       return response.data;
     } catch (err) {

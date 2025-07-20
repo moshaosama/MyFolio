ed2fefb879d3478c5accbe4f19e784032c87df0a
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 02:35:24 +0300

    feat(experience): add custom hooks for experience feature

---
+++ src/Features/Experience/Hooks/useGetExperience.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import useGetUser from "../../Portfolio/Hooks/useGetUser";
+import { experienceService } from "../../../Api/Experience/ExperienceService";
+
+const useGetExperience = () => {
+  const { User } = useGetUser();
+
+  const { data: Experience } = useQuery({
+    queryKey: ["getExperience", [User?.id]],
+    queryFn: () => experienceService.GetData(User?.id),
+    enabled: !!User?.id,
+  });
+
+  return { Experience };
+};
+
+export default useGetExperience;

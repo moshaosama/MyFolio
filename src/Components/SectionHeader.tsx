3c927d215b288fee851a4b0c60ebf65cf2f6f7de
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:22:09 +0300

    style(ui): update SectionHeader component styling

--- src/Components/SectionHeader.tsx
+++ src/Components/SectionHeader.tsx
@@ -1,5 +1,8 @@
+import { FiEdit } from "react-icons/fi";
 import cn from "../libs/cn";
 import Container from "../Styles";
+import { FaCheck } from "react-icons/fa";
+import { useOpenSkillContext } from "../Features/Skills/Context/OpenEditSkillContext";
 
 interface SectionHeaderProps {
   Title: string;
@@ -14,6 +17,7 @@ const SectionHeader = ({
   id,
   isBtn,
 }: SectionHeaderProps) => {
+  const { handleTriggerOpenEditModel, isOpenEditModel } = useOpenSkillContext();
   return (
     <div
       id={id}
@@ -29,13 +33,28 @@ const SectionHeader = ({
         {Title}
       </h1>
 
-      {isBtn && (
-        <div className="my-10 flex justify-end w-full">
-          <button className="bg-blue-500 text-white font-bold w-32 py-3 rounded-2xl hover:bg-blue-600 cursor-pointer transition-all duration-300 mx-20">
-            EditSkill
-          </button>
-        </div>
-      )}
+      {isBtn &&
+        (isOpenEditModel ? (
+          <div className="my-10 flex justify-end w-full">
+            <button
+              onClick={handleTriggerOpenEditModel}
+              className="bg-green-500 text-white flex justify-center items-center gap-3 font-bold p-3 rounded-2xl hover:bg-green-600 cursor-pointer transition-all duration-300 mx-20"
+            >
+              <FaCheck />
+              Done Edititng
+            </button>
+          </div>
+        ) : (
+          <div className="my-10 flex justify-end w-full">
+            <button
+              onClick={handleTriggerOpenEditModel}
+              className="bg-blue-500 text-white flex items-center gap-3 justify-center font-bold  p-3 rounded-2xl hover:bg-blue-600 cursor-pointer transition-all duration-300 mx-20"
+            >
+              <FiEdit />
+              Edit Skills
+            </button>
+          </div>
+        ))}
 
       <p className="text-xl font-semibold text-center text-gray-500 w-[55pc] max-sm:w-full">
         {Description}

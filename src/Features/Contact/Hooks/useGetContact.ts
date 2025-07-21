b336288df17d8911016b99dd5d7b287cbbb9daa8
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 23:18:49 +0300

    refactor: update useGetContact hook

--- src/Features/Contact/Hooks/useGetContact.ts
+++ src/Features/Contact/Hooks/useGetContact.ts
@@ -8,6 +8,7 @@ const useGetContact = () => {
     queryKey: ["getContact", [User?.id]],
     queryFn: () => contactService.getContact(User?.id),
     enabled: !!User?.id,
+    refetchOnWindowFocus: false,
   });
 
   return { refetch, Contact };

206f4ba4f527676eca0fdef0235c36fa3ef5d8fa
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 19:43:41 +0300

    feat(experience): update ExperienceRender component display

--- src/Features/Experience/Components/ExperienceRender.tsx
+++ src/Features/Experience/Components/ExperienceRender.tsx
@@ -23,7 +23,7 @@ const ExperienceRender = () => {
     <>
       <div className="flex justify-center mt-10">
         <div className="relative  flex flex-col items-center w-full max-w-5xl">
-          <div className="absolute w-1  bg-white h-full left-1/2  -translate-x-1/2 z-10" />
+          <div className="absolute w-1  bg-white max-sm:hidden h-full left-1/2  -translate-x-1/2 z-10" />
 
           {Experience?.experience?.map((experience: any, index: number) => (
             <div
@@ -37,7 +37,7 @@ const ExperienceRender = () => {
                   <>
                     <i
                       className={cn(
-                        "w-14 h-14 flex justify-center absolute  items-center bg-gray-100/30 z-50 rounded-full border-2 border-white",
+                        "w-14 h-14 max-sm:hidden flex justify-center absolute  items-center bg-gray-100/30 z-50 rounded-full border-2 border-white",
                         experience?.Position === "left" && "left-[30.2pc]"
                       )}
                     >
@@ -51,8 +51,8 @@ const ExperienceRender = () => {
                         <FaLaptopCode color="white" size={35} />
                       )}
                     </i>
-                    <div className="text-white relative bg-slate-800 p-4 rounded-lg w-full">
-                      <div className="absolute top-0 left-[27pc]">
+                    <div className="text-white relative bg-slate-800 max-sm:mx-3 p-4 rounded-lg w-full">
+                      <div className="absolute top-0 left-[27pc] max-sm:hidden">
                         <i>
                           <IoMdArrowDropright size={50} />
                         </i>
@@ -84,8 +84,8 @@ const ExperienceRender = () => {
 
                 {experience?.Position === "right" && (
                   <>
-                    <div className="text-white relative bg-slate-800 p-4 rounded-lg w-full">
-                      <div className="absolute top-0 right-[27pc]">
+                    <div className="text-white relative bg-slate-800 max-sm:mx-3 p-4 rounded-lg w-full">
+                      <div className="absolute top-0 max-sm:hidden right-[27pc]">
                         <i>
                           <IoMdArrowDropleft size={50} />
                         </i>
@@ -113,7 +113,7 @@ const ExperienceRender = () => {
                     </div>
                     <i
                       className={cn(
-                        "w-14 h-14 flex justify-center absolute  items-center bg-gray-100/30 z-50 rounded-full border-2 border-white",
+                        "w-14 h-14 flex max-sm:hidden justify-center absolute  items-center bg-gray-100/30 z-50 rounded-full border-2 border-white",
                         experience?.Position === "right" && "right-[30.2pc]"
                       )}
                     >
@@ -132,12 +132,13 @@ const ExperienceRender = () => {
               </div>
             </div>
           ))}
-
+          {/* margin: 0pc 1pc;
+width: 21.6pc; */}
           <div className="flex gap-20 ">
             {isOpenAddExperience ? null : (
               <div
                 onClick={handleTriggerOpenAddExperience}
-                className="border-2 z-50 absolute right-[23pc] border-gray-500 cursor-pointer hover:scale-105 transition-all duration-300 bg-[#0f0d14] w-72 rounded-xl flex flex-col items-center py-4 gap-4 justify-center "
+                className="border-2 z-50 absolute right-[23pc] max-sm:mx-3 max-sm:w-[21.6pc]  max-sm:right-0  border-gray-500 cursor-pointer hover:scale-105 transition-all duration-300 bg-[#0f0d14] w-72 rounded-xl flex flex-col items-center py-4 gap-4 justify-center "
               >
                 <i>
                   <IoMdAdd size={70} color="blue" />

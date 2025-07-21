98c3c5fa847ef46d44043e2a80778288eff80f3a
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 19:20:24 +0300

    feat(navbar): update Navbar component with latest changes

--- src/Layouts/Template/Navbar/index.tsx
+++ src/Layouts/Template/Navbar/index.tsx
@@ -5,29 +5,33 @@ const NavbarTemplate = () => {
   const { NavbarLinks, handleDeleteLink } = useNavbarLinks();
   const rawLinks = NavbarLinks?.result?.[0]?.Links;
 
+  let links: any[] = [];
+
+  try {
+    links = typeof rawLinks === "string" ? JSON.parse(rawLinks) : rawLinks;
+  } catch (err) {
+    console.error("Failed to parse links", err);
+  }
+
   return (
     <>
       <div className="bg-[#0f0d14] max-sm:hidden w-full relative  flex justify-center gap-5">
         <div className="flex justify-center gap-20 py-4 px-14 my-4 rounded-full sticky top-0 bg-[#211f2a]">
-          {rawLinks
-            ? JSON.parse(rawLinks).map(
-                (data: { name: string; to: string }, index: number) => (
-                  <a href={data.to} key={index}>
-                    <div className="text-gray-500 flex group">
-                      <h1 className="text-xl font-semibold hover:text-white transition-all duration-500 cursor-pointer">
-                        {data.name}
-                      </h1>
-                      <i
-                        onClick={() => handleDeleteLink(data.name)}
-                        className="hidden hover:text-white transition-all duration-300 cursor-pointer group-hover:inline-block"
-                      >
-                        <MdDelete size={20} />
-                      </i>
-                    </div>
-                  </a>
-                )
-              )
-            : null}
+          {links?.map((data: { name: string; to: string }, index: number) => (
+            <a href={data.to} key={index}>
+              <div className="text-gray-500 flex group">
+                <h1 className="text-xl font-semibold hover:text-white transition-all duration-500 cursor-pointer">
+                  {data.name}
+                </h1>
+                <i
+                  onClick={() => handleDeleteLink(data.name)}
+                  className="hidden hover:text-white transition-all duration-300 cursor-pointer group-hover:inline-block"
+                >
+                  <MdDelete size={20} />
+                </i>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </>

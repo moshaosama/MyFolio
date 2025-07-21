983bf10f926c7b089c798bfc566076743ba7d404
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 18:49:22 +0300

    feat(ui): enhance contact header component

--- src/Features/Contact/Components/ContactHeader.tsx
+++ src/Features/Contact/Components/ContactHeader.tsx
@@ -2,7 +2,7 @@ import SectionHeader from "../../../Components/SectionHeader";
 
 const ContactHeader = () => {
   return (
-    <div className="pt-40">
+    <div className="pt-40 mt-20">
       <SectionHeader
         Description="I'm always open to new opportunities and collaborations. Feel free to reach out through any of these channels."
         Title="Get In Touch"

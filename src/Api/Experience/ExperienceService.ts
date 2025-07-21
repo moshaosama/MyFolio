a79122b5641d6a2fcaa83cdccb298620d8e4ad65
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 14:57:02 +0300

    refactor(api): update experience service integration

--- src/Api/Experience/ExperienceService.ts
+++ src/Api/Experience/ExperienceService.ts
@@ -2,11 +2,52 @@ import axios from "axios";
 import { ParentService } from "../parentService";
 
 class ExperienceService extends ParentService {
-  async CreateData(data?: any, thunkApi?: any) {}
+  async CreateData(
+    data?: {
+      Date: string;
+      Title: string;
+      Foundation: string;
+      Description: string;
+      Icon: string;
+      Position: string;
+      user_id: number;
+    },
+    thunkApi?: any
+  ) {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/create-experience/${data?.user_id}`,
+        {
+          Date: data?.Date,
+          Title: data?.Title,
+          Foundation: data?.Foundation,
+          Description: data?.Description,
+          Icon: data?.Icon,
+          Position: data?.Position,
+        }
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
   async GetData(userId?: number, thunkApi?: any) {
     try {
       const response = await axios.get(
         `http://localhost:3000/get-experience/${userId}`
+      );
+
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async DeleteExperience(experience_id?: number, thunkApi?: any) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/delete-experience/${experience_id}`
       );
 
       return response.data;

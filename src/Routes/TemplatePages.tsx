e6e5216cb0014f1afcd5eaa583ed774efedee92d
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 18:49:42 +0300

    refactor(routes): update route template pages

--- src/Routes/TemplatePages.tsx
+++ src/Routes/TemplatePages.tsx
@@ -1,12 +1,12 @@
 import SplitLayoutTemplate from "../Layouts/Template/SplitLayout";
 import Template from "../Pages/Templates";
-// import WithLoader from "../Utils/WithLoader";
+import WithLoader from "../Utils/WithLoader";
 
-// const SplitLayoutLoader = WithLoader(SplitLayoutTemplate);
+const SplitLayoutLoader = WithLoader(SplitLayoutTemplate);
 
 export const TemplatePages = {
   path: "template",
-  element: <SplitLayoutTemplate />,
+  element: <SplitLayoutLoader />,
   children: [
     {
       index: true,

a312f4724744f8f8bd74128220769050f30e861e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 19:43:43 +0300

    feat(about): improve About section content or layout

--- src/Features/Portfolio/Components/About.tsx
+++ src/Features/Portfolio/Components/About.tsx
@@ -12,14 +12,6 @@ const About = () => {
 
   let parsedTags: string[] = [];
 
-  // try {
-  //   if (User?.Tags) {
-  //     parsedTags = JSON.parse(JSON.parse(User.Tags));
-  //   }
-  // } catch (err) {
-  //   console.error("Error parsing Tags", err);
-  // }
-
   try {
     if (User?.Tags) {
       parsedTags = JSON.parse(User.Tags);
@@ -48,13 +40,13 @@ const About = () => {
 
       <div className="mt-3">
         <h1 className="text-white text-4xl font-bold">Hello I am</h1>
-        <div className="w-96 flex break-words mt-2">
+        <div className="w-96 max-sm:w-20 flex break-words mt-2">
           <form>
             <input
               defaultValue={User?.Name}
               {...register("Name", { required: true })}
               onMouseLeave={handleSubmit(handlEditName)}
-              className="text-5xl max-sm:text-3xl font-extrabold bg-gradient-to-l bg-clip-text w-[26pc] max-sm:w-full from-[#2a7b9b] to-[#57c785] text-transparent focus:outline-0 focus:border-b-2 focus:border-b-purple-400 wrap-break-word"
+              className="text-5xl max-sm:text-3xl font-extrabold bg-gradient-to-l bg-clip-text w-[26pc] max-sm:w-20 from-[#2a7b9b] to-[#57c785] text-transparent focus:outline-0 focus:border-b-2 focus:border-b-purple-400 wrap-break-word"
             />
           </form>
 

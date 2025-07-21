cf3e926bd57f01568175310fb301c36efb909ede
moshaosama <mohamedOSFekry@gmail.com>
2025-07-22 00:08:54 +0300

    Latest Version

--- src/Features/Home/Hooks/useCreateUser.ts
+++ src/Features/Home/Hooks/useCreateUser.ts
@@ -1,11 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { userService } from "../../../Api/User/UserService";
-import { navbarService } from "../../../Api/Navbar/NavbarService";
 import { useNavigate } from "react-router-dom";
-import useGetUser from "../../Portfolio/Hooks/useGetUser";
 
 const useCreateUser = () => {
-  const { User } = useGetUser();
   const { mutateAsync: CreateUser } = useMutation({
     mutationKey: ["createUser"],
     mutationFn: () => userService.CreateData(),
@@ -21,16 +18,10 @@ const useCreateUser = () => {
     },
   });
 
-  const { mutateAsync: CreateLinksByUser } = useMutation({
-    mutationKey: ["createLink"],
-    mutationFn: navbarService.CreateData,
-  });
-
   const navigate = useNavigate();
 
   const handleCreateUser = async () => {
     await CreateUser();
-    await CreateLinksByUser(User?.id);
     navigate("/template");
   };
   return { handleCreateUser };

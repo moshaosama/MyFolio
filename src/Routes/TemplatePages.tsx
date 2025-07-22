b5d0673461b0d746e7c531dc6741d656db149d14
moshaosama <mohamedOSFekry@gmail.com>
2025-07-22 23:11:15 +0300

    chore: update template routing logic

--- src/Routes/TemplatePages.tsx
+++ src/Routes/TemplatePages.tsx
@@ -2,11 +2,11 @@ import SplitLayoutTemplate from "../Layouts/Template/SplitLayout";
 import Template from "../Pages/Templates";
 import WithLoader from "../Utils/WithLoader";
 
-const SplitLayoutLoader = WithLoader(SplitLayoutTemplate);
+// const SplitLayoutLoader = WithLoader(SplitLayoutTemplate);
 
 export const TemplatePages = {
   path: "template",
-  element: <SplitLayoutLoader />,
+  element: <SplitLayoutTemplate />,
   children: [
     {
       index: true,

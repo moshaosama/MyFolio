2f8afbfced7f22a45e83ac3fbf17bc2400437fe5
moshaosama <mohamedOSFekry@gmail.com>
2025-07-23 16:15:05 +0300

    feat: update PopUpChat UI and logic

--- src/Features/Chatgbt/Components/PopUpChat.tsx
+++ src/Features/Chatgbt/Components/PopUpChat.tsx
@@ -1,17 +1,20 @@
 import { CgClose } from "react-icons/cg";
 import { LuBot } from "react-icons/lu";
 import useOpenChatBot from "../Hooks/useOpenChatBot";
+import { SyncLoader } from "react-spinners";
 
 const PopUpChat = () => {
   const {
     handleTriggerChatBot,
     isOpenChatBot,
     handleStartChat,
-    StartChat,
+    isResponse,
     register,
     handleSubmit,
     handleSendMessage,
+    StartChat,
     SendMessages,
+    ChatMessage,
   } = useOpenChatBot();
 
   return (
@@ -30,7 +33,7 @@ const PopUpChat = () => {
         )}
 
         {isOpenChatBot && (
-          <div className="border-2 rounded-xl z-50  text-white fixed right-10 bottom-24 border-gray-500 w-[30pc]">
+          <div className="border-2 rounded-xl z-50  text-white fixed right-10 bottom-24 border-gray-500 w-[30pc] overflow-y-scroll h-[30pc]">
             <div className="bg-black rounded-t-lg">
               <div className="mx-3  flex justify-between items-center py-4">
                 <div>
@@ -42,33 +45,45 @@ const PopUpChat = () => {
               </div>
             </div>
 
-            <div className="bg-gray-500 rounded-b-lg py-3 flex flex-col gap-5">
-              <div className="mx-3 bg-white relative left-0 text-black py-3 rounded-lg px-2 w-1/2">
-                <div>{JSON.parse(StartChat!)?.message}</div>
-              </div>
-
-              <div className="mx-3 bg-white relative left-52 text-black py-3 rounded-lg px-2 w-1/2">
-                <div>
-                  {SendMessages?.map((message) => (
-                    <h1>{message.message}</h1>
-                  ))}
+            <div className="bg-gray-500 relative rounded-b-lg py-3 flex flex-col gap-5 h-[22pc] overflow-y-scroll ">
+              <div className="flex flex-col gap-5">
+                <div className="mx-3 bg-white relative left-0 text-black py-3 rounded-lg px-2 w-1/2">
+                  <div>{JSON.parse(StartChat!)?.message}</div>
                 </div>
-              </div>
-
-              <form onSubmit={handleSubmit(handleSendMessage)}>
-                <p className="flex gap-3 mx-1">
-                  <input
-                    type="text"
-                    placeholder="Enter Your text.."
-                    className="p-2 text-black bg-white rounded-lg w-full"
-                    {...register("message", { required: true })}
-                  />
-                  <button className="bg-green-500 rounded-xl font-bold cursor-pointer hover:bg-green-700 transition-all duration-300 w-20">
-                    Send
-                  </button>
-                </p>
-              </form>
+                {SendMessages.length >= 1 &&
+                  SendMessages?.map((message) => (
+                    <div className="self-end bg-white mx-3 text-black py-3 rounded-lg px-2 w-1/2">
+                      <h1>{message.message}</h1>
+                    </div>
+                  ))}
+                {isResponse ? (
+                  <SyncLoader />
+                ) : (
+                  ChatMessage.length >= 2 &&
+                  ChatMessage?.slice(1)?.map((message) => (
+                    <div className="self-start mx-3 bg-white text-black py-3 rounded-lg px-2 w-1/2">
+                      <h1>{message.message}</h1>
+                    </div>
+                  ))
+                )}
+              </div>{" "}
             </div>
+            <form
+              className="fixed bottom-[6.5pc] w-[28pc]"
+              onSubmit={handleSubmit(handleSendMessage)}
+            >
+              <p className="flex gap-3 mx-1">
+                <input
+                  type="text"
+                  placeholder="Enter Your text.."
+                  className="py-3 px-2 text-black bg-white rounded-lg w-full"
+                  {...register("message", { required: true })}
+                />
+                <button className="bg-green-500 rounded-xl font-bold cursor-pointer hover:bg-green-700 transition-all duration-300 w-20">
+                  Send
+                </button>
+              </p>
+            </form>
           </div>
         )}
       </div>

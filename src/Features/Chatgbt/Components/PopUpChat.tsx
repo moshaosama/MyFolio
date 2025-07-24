48960a93ef156bd6017c1bea143a21da20dcc6ca
moshaosama <mohamedOSFekry@gmail.com>
2025-07-24 18:25:41 +0300

    Latest Version

--- src/Features/Chatgbt/Components/PopUpChat.tsx
+++ src/Features/Chatgbt/Components/PopUpChat.tsx
@@ -33,7 +33,7 @@ const PopUpChat = () => {
         )}
 
         {isOpenChatBot && (
-          <div className="border-2 rounded-xl z-50  text-white fixed right-10 bottom-24 border-gray-500 w-[30pc] overflow-y-scroll h-[30pc]">
+          <div className="border-2 rounded-xl z-50  text-white fixed right-10 max-sm:right-2 bottom-24 border-gray-500 w-[30pc] max-sm:w-[25pc] overflow-y-scroll h-[30pc]">
             <div className="bg-black rounded-t-lg">
               <div className="mx-3  flex justify-between items-center py-4">
                 <div>
@@ -47,7 +47,7 @@ const PopUpChat = () => {
 
             <div className="bg-gray-500 relative rounded-b-lg py-3 flex flex-col gap-5 h-[22pc] overflow-y-scroll ">
               <div className="flex flex-col gap-5">
-                <div className="mx-3 bg-white relative left-0 text-black py-3 rounded-lg px-2 w-1/2">
+                <div className="mx-3 max-sm:mx-0 bg-white relative left-0 text-black py-3 rounded-lg px-2 w-1/2">
                   <div>{JSON.parse(StartChat!)?.message}</div>
                 </div>
                 {SendMessages.length >= 1 &&
@@ -69,7 +69,7 @@ const PopUpChat = () => {
               </div>{" "}
             </div>
             <form
-              className="fixed bottom-[6.5pc] w-[28pc]"
+              className="fixed bottom-[6.5pc] w-[28pc] max-sm:w-[23pc] bg-black"
               onSubmit={handleSubmit(handleSendMessage)}
             >
               <p className="flex gap-3 mx-1">
